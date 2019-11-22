cfa47b9bd69ae8982bd3eee95aed5c226359b90a
Amaljith Kavungalparambil Ayyappan [Student] <amaljith.kavungalparambilayyappan@student.sl.on.ca>
2019-11-22 10:30:20 -0500

    Added Hazard Properties

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.transform.tag == "Player")
+        {
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+            Instantiate(playerDeathPrefab, coll.contacts[0].point, Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+            Destroy(coll.gameObject);
+           // GameManager.instance.RestartLevel(1.25f);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
